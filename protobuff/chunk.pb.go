1a6861628ed4556cc20934a3ca1369ccb8225b6b
Fahim Shariar <fahim.shoumik@gmail.com>
2020-10-05 00:28:04 +0600

    is admin

--- protobuff/chunk.pb.go
+++ protobuff/chunk.pb.go
@@ -25,7 +25,7 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
+const _ = proto.GoGoProtoPackageIsVersion2 // please upgrade the proto package
 
 type BigFileChunk struct {
 	// Fine Name
@@ -1227,7 +1227,6 @@ func (m *BigFileAck) Unmarshal(dAtA []byte) error {
 func skipChunk(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
-	depth := 0
 	for iNdEx < l {
 		var wire uint64
 		for shift := uint(0); ; shift += 7 {
@@ -1259,8 +1258,10 @@ func skipChunk(dAtA []byte) (n int, err error) {
 					break
 				}
 			}
+			return iNdEx, nil
 		case 1:
 			iNdEx += 8
+			return iNdEx, nil
 		case 2:
 			var length int
 			for shift := uint(0); ; shift += 7 {
@@ -1281,30 +1282,55 @@ func skipChunk(dAtA []byte) (n int, err error) {
 				return 0, ErrInvalidLengthChunk
 			}
 			iNdEx += length
-		case 3:
-			depth++
-		case 4:
-			if depth == 0 {
-				return 0, ErrUnexpectedEndOfGroupChunk
+			if iNdEx < 0 {
+				return 0, ErrInvalidLengthChunk
 			}
-			depth--
+			return iNdEx, nil
+		case 3:
+			for {
+				var innerWire uint64
+				var start int = iNdEx
+				for shift := uint(0); ; shift += 7 {
+					if shift >= 64 {
+						return 0, ErrIntOverflowChunk
+					}
+					if iNdEx >= l {
+						return 0, io.ErrUnexpectedEOF
+					}
+					b := dAtA[iNdEx]
+					iNdEx++
+					innerWire |= (uint64(b) & 0x7F) << shift
+					if b < 0x80 {
+						break
+					}
+				}
+				innerWireType := int(innerWire & 0x7)
+				if innerWireType == 4 {
+					break
+				}
+				next, err := skipChunk(dAtA[start:])
+				if err != nil {
+					return 0, err
+				}
+				iNdEx = start + next
+				if iNdEx < 0 {
+					return 0, ErrInvalidLengthChunk
+				}
+			}
+			return iNdEx, nil
+		case 4:
+			return iNdEx, nil
 		case 5:
 			iNdEx += 4
+			return iNdEx, nil
 		default:
 			return 0, fmt.Errorf("proto: illegal wireType %d", wireType)
 		}
-		if iNdEx < 0 {
-			return 0, ErrInvalidLengthChunk
-		}
-		if depth == 0 {
-			return iNdEx, nil
-		}
 	}
-	return 0, io.ErrUnexpectedEOF
+	panic("unreachable")
 }
 
 var (
-	ErrInvalidLengthChunk        = fmt.Errorf("proto: negative length found during unmarshaling")
-	ErrIntOverflowChunk          = fmt.Errorf("proto: integer overflow")
-	ErrUnexpectedEndOfGroupChunk = fmt.Errorf("proto: unexpected end of group")
+	ErrInvalidLengthChunk = fmt.Errorf("proto: negative length found during unmarshaling")
+	ErrIntOverflowChunk   = fmt.Errorf("proto: integer overflow")
 )
