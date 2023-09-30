931fa4fe2872c07c5e307a02060e39c9d54601ab
fahim <fahim.shoumik@gmail.com>
2023-10-01 02:16:19 +0600

    dataloader struct added

--- interfaces/gqlgen_server.go
+++ interfaces/gqlgen_server.go
@@ -2,9 +2,15 @@ package interfaces
 
 import (
 	"context"
+	"github.com/graph-gophers/dataloader/v7"
 	"github.com/labstack/echo/v4"
 	"github.com/vektah/gqlparser/v2/ast"
 )
+
+type DataLoaders struct {
+	MultiLoader  *dataloader.Loader[string, interface{}]
+	SingleLoader *dataloader.Loader[string, interface{}]
+}
 
 type GqlServer interface {
 	SolvePublicQuery(ctx context.Context, model string, _args map[string]interface{}, selectionSet *ast.SelectionSet, router echo.Context) ([]byte, error)
