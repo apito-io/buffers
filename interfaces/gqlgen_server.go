4eced48254293b1eb1995c8e8edc8f038a6030b4
fahim <fahim.shoumik@gmail.com>
2023-10-04 17:37:51 +0600

    marshal & unmarshal changed

--- interfaces/gqlgen_server.go
+++ interfaces/gqlgen_server.go
@@ -16,8 +16,8 @@ type SchemaLoaderInterface interface {
 
 // DataLoaders Dataloaders
 type DataLoaders struct {
-	MultiLoader  *dataloader.Loader[string, interface{}]
-	SingleLoader *dataloader.Loader[string, interface{}]
+	MultiLoader *dataloader.Loader[string, interface{}]
+	//SingleLoader *dataloader.Loader[string, interface{}]
 }
 
 // GqlServer to Gql connect functions
@@ -37,13 +37,25 @@ type Result[T any] struct {
 	Code    int  `json:"code"`
 }
 
+type DataloaderResult[T any] struct {
+	Result  []T  `json:"result"`
+	Count   int  `json:"count"`
+	Cached  bool `json:"cached"`
+	HasMore bool `json:"hasMore"`
+	Error   bool `json:"error"`
+	Code    int  `json:"code"`
+}
+
 func UnmarshalDynamicCount[T any](body []byte) (*T, error) {
 	var res Result[T]
 	err := json.Unmarshal(body, &res)
 	if err != nil {
 		return nil, err
 	}
-	return res.Result[0], nil
+	if res.Count > 0 {
+		return res.Result[0], nil
+	}
+	return nil, nil
 }
 
 func UnmarshalDynamicResults[T any](body []byte) ([]*T, error) {
@@ -52,7 +64,10 @@ func UnmarshalDynamicResults[T any](body []byte) ([]*T, error) {
 	if err != nil {
 		return nil, err
 	}
-	return res.Result, nil
+	if res.Count > 0 {
+		return res.Result, nil
+	}
+	return nil, nil
 }
 
 func UnmarshalDynamicResult[T any](body []byte) (*T, error) {
@@ -61,5 +76,8 @@ func UnmarshalDynamicResult[T any](body []byte) (*T, error) {
 	if err != nil {
 		return nil, err
 	}
-	return res.Result[0], nil
+	if res.Count > 0 {
+		return res.Result[0], nil
+	}
+	return nil, nil
 }
