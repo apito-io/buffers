125e817ec7aad49ace1835540bda543050cb2efe
fahim <fahim.shoumik@gmail.com>
2024-04-29 11:18:42 +0600

    uploaded full path var added to filedetails

--- extensions/extension.go
+++ extensions/extension.go
@@ -22,19 +22,20 @@ type EnvVariables struct {
 }
 
 type FileDetails struct {
-	Id             string        `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty" firestore:"id,omitempty"`
-	XKey           string        `protobuf:"bytes,2,opt,name=_key,json=Key,proto3" json:"_key,omitempty" firestore:"_key,omitempty"`
-	Type           string        `protobuf:"bytes,3,opt,name=type,proto3" json:"type,omitempty" firestore:"type,omitempty"`
-	FileExtension  string        `protobuf:"bytes,4,opt,name=file_extension,json=fileExtension,proto3" json:"file_extension,omitempty" file_extension:"title,omitempty"`
-	FileName       string        `protobuf:"bytes,5,opt,name=file_name,json=fileName,proto3" json:"file_name,omitempty" firestore:"file_name,omitempty"`
-	RemoteFilePath string        `protobuf:"bytes,5,opt,name=remote_file_path,json=remote_file_path,proto3" json:"remote_file_path,omitempty" firestore:"remote_file_path,omitempty"`
-	ContentType    string        `protobuf:"bytes,6,opt,name=content_type,json=contentType,proto3" json:"content_type,omitempty" firestore:"content_type,omitempty"`
-	Size           int64         `protobuf:"varint,7,opt,name=size,proto3" json:"size,omitempty" firestore:"size,omitempty"`
-	S3Key          string        `protobuf:"bytes,8,opt,name=s3_key,json=s3Key,proto3" json:"s3_key,omitempty" firestore:"s3_key,omitempty"`
-	Url            string        `protobuf:"bytes,9,opt,name=url,proto3" json:"url,omitempty" firestore:"url,omitempty"`
-	CreatedAt      string        `protobuf:"bytes,10,opt,name=created_at,json=createdAt,proto3" json:"created_at,omitempty" firestore:"created_at,omitempty"`
-	UploadParam    *UploadParams `protobuf:"bytes,11,opt,name=upload_param,json=uploadParam,proto3" json:"upload_param,omitempty" firestore:"upload_param,omitempty"`
-	Buffer         []byte        `protobuf:"bytes,12,opt,name=buffer,proto3" json:"buffer,omitempty" firestore:"upload_param,omitempty"`
+	Id               string        `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty" firestore:"id,omitempty"`
+	XKey             string        `protobuf:"bytes,2,opt,name=_key,json=Key,proto3" json:"_key,omitempty" firestore:"_key,omitempty"`
+	Type             string        `protobuf:"bytes,3,opt,name=type,proto3" json:"type,omitempty" firestore:"type,omitempty"`
+	FileExtension    string        `protobuf:"bytes,4,opt,name=file_extension,json=fileExtension,proto3" json:"file_extension,omitempty" file_extension:"title,omitempty"`
+	FileName         string        `protobuf:"bytes,5,opt,name=file_name,json=fileName,proto3" json:"file_name,omitempty" firestore:"file_name,omitempty"`
+	RemoteFilePath   string        `protobuf:"bytes,5,opt,name=remote_file_path,json=remote_file_path,proto3" json:"remote_file_path,omitempty" firestore:"remote_file_path,omitempty"`
+	UploadedFullPath string        `protobuf:"bytes,6,opt,name=uploaded_full_path,json=uploaded_full_path,proto3" json:"uploaded_full_path,omitempty" firestore:"uploaded_full_path,omitempty"`
+	ContentType      string        `protobuf:"bytes,6,opt,name=content_type,json=contentType,proto3" json:"content_type,omitempty" firestore:"content_type,omitempty"`
+	Size             int64         `protobuf:"varint,7,opt,name=size,proto3" json:"size,omitempty" firestore:"size,omitempty"`
+	S3Key            string        `protobuf:"bytes,8,opt,name=s3_key,json=s3Key,proto3" json:"s3_key,omitempty" firestore:"s3_key,omitempty"`
+	Url              string        `protobuf:"bytes,9,opt,name=url,proto3" json:"url,omitempty" firestore:"url,omitempty"`
+	CreatedAt        string        `protobuf:"bytes,10,opt,name=created_at,json=createdAt,proto3" json:"created_at,omitempty" firestore:"created_at,omitempty"`
+	UploadParam      *UploadParams `protobuf:"bytes,11,opt,name=upload_param,json=uploadParam,proto3" json:"upload_param,omitempty" firestore:"upload_param,omitempty"`
+	Buffer           []byte        `protobuf:"bytes,12,opt,name=buffer,proto3" json:"buffer,omitempty" firestore:"upload_param,omitempty"`
 }
 
 type UploadParams struct {
