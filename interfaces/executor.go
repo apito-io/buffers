4dd4dbb48f91b82d4d2fdccf4ba0fc3d01cce21d
fahim <fahim.shoumik@gmail.com>
2024-04-29 11:40:15 +0600

    media function transfered from executor interface

--- interfaces/executor.go
+++ interfaces/executor.go
@@ -34,6 +34,6 @@ type GraphQLExecutorInterface interface {
 	ConnectDisconnectParamBuilder(ctx context.Context, schema *protobuff.ProjectSchema, uid string, collectionName string, connectionIds map[string]interface{}, modelType *protobuff.ModelType) ([]*shared.ConnectDisconnectParam, error)
 	HandlePayloadFormatting(ctx context.Context, param *shared.CommonSystemParams, isFaker bool, local string, fields []*protobuff.FieldInfo, inputPayload map[string]interface{}, dbPayload map[string]interface{}) (map[string]interface{}, error)
 
-	UploadImageFromURL(ctx context.Context, projectId, modelName, imageUrl string) (*protobuff.FileDetails, error)
-	HandleMediaURL(ctx context.Context, param *shared.CommonSystemParams, media map[string]interface{}) (interface{}, error)
+	//UploadImageFromURL(ctx context.Context, projectId, modelName, imageUrl string) (*protobuff.FileDetails, error)
+	//HandleMediaURL(ctx context.Context, param *shared.CommonSystemParams, media map[string]interface{}) (interface{}, error)
 }
