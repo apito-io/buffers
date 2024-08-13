454e8c33ed6ec9b1ddb8816c167c1d9f77f15dfc
fahim <fahim.shoumik@gmail.com>
2024-08-13 15:30:44 +0600

    system interface cleanup

--- interfaces/system_db.go
+++ interfaces/system_db.go
@@ -16,9 +16,6 @@ type SystemDBInterface interface {
 	// GetOrganizations retrieves multiple user organizations by their IDs
 	GetOrganizations(ctx context.Context, userId string) (*shared.SearchResponse[protobuff.Organization], error)
 
-	// ListTeams lists all the teams for a given project
-	//ListTeams(ctx context.Context, param *shared.CommonSystemParams) ([]*protobuff.SystemUser, error)
-
 	// GetSystemUser retrieves a system user by their ID
 	GetSystemUser(ctx context.Context, id string) (*protobuff.SystemUser, error)
 
@@ -27,47 +24,17 @@ type SystemDBInterface interface {
 	// FindOrganizationAdmin retrieves an organization admin by their ID
 	FindOrganizationAdmin(ctx context.Context, orgId string) (*protobuff.SystemUser, error)
 
-	// GetSystemUsers retrieves multiple system users by their IDs
-	//GetSystemUsers(ctx context.Context, keys []string) (map[string]*protobuff.SystemUser, error)
-
-	// CreateSystemUser creates a new system user
-	CreateSystemUser(ctx context.Context, user *protobuff.SystemUser) (*protobuff.SystemUser, error)
-
 	// UpdateSystemUser updates a system user's profile
 	UpdateSystemUser(ctx context.Context, user *protobuff.SystemUser, replace bool) error
 
 	// SearchResource searches for system users based on a filter
 	SearchResource(ctx context.Context, param *shared.CommonSystemParams) (*shared.SearchResponse[any], error)
 
-	// SearchUsers searches for system users based on a filter
-	SearchUsers(ctx context.Context, param *shared.CommonSystemParams) (*shared.SearchResponse[protobuff.SystemUser], error)
-
-	// GetProjectWithRolesAndPermission lists all the projects for a given user
-	//GetProjectWithRolesAndPermission(ctx context.Context, userId string) ([]*protobuff.ProjectWithRoles, error)
-
-	// ListAllUsers lists all system users
-	//ListAllUsers(ctx context.Context) ([]*protobuff.SystemUser, error)
-
 	// GetProject retrieves a project by its ID
 	GetProject(ctx context.Context, id string) (*protobuff.Project, error)
 
-	// CheckProjectName checks if a project name already exists
-	CheckProjectName(ctx context.Context, name string) error
-
-	// ListProjects lists all the projects for a given user
-	ListProjects(ctx context.Context, param *shared.CommonSystemParams) (*shared.SearchResponse[protobuff.Project], error)
-
-	// ListAllProjects lists all the projects for a given user
-	//ListAllProjects(ctx context.Context, userId string) ([]*protobuff.Project, error)
-
 	// ListFunctions lists all the cloud functions for a given project
 	ListFunctions(ctx context.Context, param *shared.CommonSystemParams) (*shared.SearchResponse[protobuff.CloudFunction], error)
-
-	// AddSystemUserMetaInfo adds metadata to a system user
-	AddSystemUserMetaInfo(ctx context.Context, doc *shared.DefaultDocumentStructure) (*shared.DefaultDocumentStructure, error)
-
-	// CreateProject creates a new project
-	CreateProject(ctx context.Context, project *protobuff.Project) (*protobuff.Project, error)
 
 	// UpdateProject updates a project
 	UpdateProject(ctx context.Context, project *protobuff.Project, replace bool) error
